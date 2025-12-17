c8714716ce384093b904e52c4632127f5ea3a630
giftbene6-jpg <giftbene6-jpg@users.noreply.github.com>
2025-12-17 03:00:52 -0800

    Remove Facebook link from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -38,24 +38,7 @@ const Footer = () => {
 
           {/* Social Links */}
           <div className="flex gap-6 items-center">
-            <Link
-              href="https://www.facebook.com/share/1FwqQB1Z6T/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-400 hover:text-blue-500 transition-colors transform hover:scale-110 duration-200"
-              aria-label="Facebook"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-8 h-8"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-              </svg>
-            </Link>
+
 
             <Link
               href="https://twitter.com"
